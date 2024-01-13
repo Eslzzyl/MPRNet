f0c89be25f766eb9bd1b63ac069243876cbb9089
Eslzzyl <eslzzyl@163.com>
2024-01-13 11:05:19 +0800

    update

---
+++ demo_deraining.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# Rain100H
+python demo.py \
+    --input_dir ./Deraining/Datasets/test/Rain100H/input \
+    --result_dir ./Deraining/results/Rain100H \
+    --task Deraining
+
+# Rain100L
+python demo.py \
+    --input_dir ./Deraining/Datasets/test/Rain100L/input \
+    --result_dir ./Deraining/results/Rain100L \
+    --task Deraining
